54898d1d93009c9dfdd217126de8800dba9df3a4
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-06-13 15:02:39 +0200

    basic csv with infos

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import requests
+import pandas as pd
+import json
 
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from utils import create_url
 
+REGION = "europe"       # Régional
+PLATFORM = "euw1"       # Plateforme
+SUMMONER_NAME = "Liquid Platypus"
+TAG_LINE = "FEET"
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+player_data = requests.get(**create_url(
+    "/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}",
+    region=REGION,
+    gameName=SUMMONER_NAME,
+    tagLine=TAG_LINE
+)).json()
 
+player_mastery = requests.get(**create_url(
+    "/lol/champion-mastery/v4/champion-masteries/by-puuid/{encryptedPUUID}",
+    region=PLATFORM,
+    encryptedPUUID=player_data["puuid"]
+)).json()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+with open("dragontail-16.12.1/16.12.1/data/fr_FR/champion.json") as f:
+    champions_raw = json.load(f)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+champion_id_to_name = {
+    int(champ["key"]): champ["name"]
+    for champ in champions_raw["data"].values()
+}
+
+df = pd.json_normalize(player_mastery)
+df["championName"] = df["championId"].map(champion_id_to_name)
+
+df.drop(columns=["championId"], inplace=True)
+df.drop(columns=["puuid"], inplace=True)
+df = df.iloc[:, [-1] + list(range(df.shape[1] - 1))]
+df.to_csv("player_mastery.csv", index=False)

f3cf014e5122b5b9f3a746f305bd18b7697b2ff4
Liquid Platypus <104024934+LiquidPlatypus@users.noreply.github.com>
2026-08-19 15:52:52 +0200

    reverted to champ ID and new func

--- backend/data/processor.py
+++ backend/data/processor.py
@@ -5,11 +5,14 @@ def process_mastery(mastery_raw, champion_id_to_name):
     df["championName"] = df["championId"].map(champion_id_to_name).fillna("Unknown")
     df = df.fillna(0)
     df["lastPlayTime"] = pd.to_datetime(df["lastPlayTime"], unit="ms").dt.strftime("%d-%m-%Y %H:%M:%S")
-    df.drop(columns=["championId", "puuid"], inplace=True)
+    df.drop(columns=["puuid"], inplace=True)
 
     # Mettre championName en première colonne
     cols = ["championName"] + [c for c in df.columns if c != "championName"]
     return df[cols]
+
+def get_top_champs(df_mastery, nb_to_show = 5):
+    return df_mastery.nlargest(nb_to_show, "championPoints")
 
 def process_matches(matches_raw):
     return pd.json_normalize(matches_raw)

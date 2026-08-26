6389c24ca795f69cd876ab8c122acd45ee078045
Liquid Platypus <thibaut.bournonville@gmail.com>
2026-08-26 14:41:51 +0200

    added css to search bar

--- frontend/src/components/SearchBar.tsx
+++ frontend/src/components/SearchBar.tsx
@@ -1,34 +1,40 @@
-"use client";
+	"use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+	import { useState } from "react";
+	import { useRouter } from "next/navigation";
 
-export default function Searchbar() {
-	const [gameName, setGameName] = useState("");
-	const [tagLine, setTagLine] = useState("");
+	import styles from "./SearchBar.module.css";
 
-	const router = useRouter();
+	export default function Searchbar() {
+		const [gameName, setGameName] = useState("");
+		const [tagLine, setTagLine] = useState("");
 
-	return (
-		<form
-			onSubmit={e => {
-				e.preventDefault();
-				router.push(`/profile?gameName=${gameName}&tagLine=${tagLine}`);
-			}}
-		>
-			<input
-				type="text"
-				placeholder="Username"
-				value={gameName}
-				onChange={e => setGameName(e.target.value)}
-			/>
-			<input
-				type="text"
-				placeholder="Tagline"
-				value={tagLine}
-				onChange={e => setTagLine(e.target.value)}
-			/>
-			<button type="submit">Search</button> {/* TODO: remplacer 'Search' by csv */}
-		</form>
-	);
-}
+		const router = useRouter();
+
+		return (
+			<div>
+				<form
+					onSubmit={e => {
+						e.preventDefault();
+						router.push(`/profile?gameName=${gameName}&tagLine=${tagLine}`);
+					}}
+				>
+					<input
+						type="text"
+						placeholder="Username"
+						value={gameName}
+						onChange={e => setGameName(e.target.value)}
+						className={styles.gameNameInput}
+					/>
+					<input
+						type="text"
+						placeholder="Tagline"
+						value={tagLine}
+						onChange={e => setTagLine(e.target.value)}
+						className={styles.tagLineInput}
+					/>
+					<button type="submit">Search</button> {/* TODO: remplacer 'Search' by csv */}
+				</form>
+			</div>
+		);
+	}

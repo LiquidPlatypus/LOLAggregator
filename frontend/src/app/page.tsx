3b7f02929b47efb7cfafbfe3285197a5447a783e
Liquid Platypus <thibaut.bournonville@gmail.com>
2026-08-28 11:28:19 +0200

    little css for blur and centering on main page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,9 +6,13 @@ import Searchbar from "@/components/SearchBar";
 export default function Home() {
 	return (
 		<div className={styles.page}>
-			<main className={styles.main}>
-				<Searchbar></Searchbar>
-			</main>
+			<div className={styles.blurOverlay}>
+				<main className={styles.main}>
+					<div className={styles.searchBar}>
+						<Searchbar></Searchbar>
+					</div>
+				</main>
+			</div>
 		</div>
 	);
 }

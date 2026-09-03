68006a394698aa7a6b7afaf709fa2dd477659bf1
Liquid Platypus <thibaut.bournonville@gmail.com>
2026-09-03 10:51:54 +0200

    champ page

---
+++ frontend/src/app/champion/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function ChampionPage() {
+	return (
+		<div className={styles.championContainer}>
+			<h1>Champion Page</h1>
+		</div>
+	);
+}
